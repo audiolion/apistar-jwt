969d3030edc4d794bda367cce96b1fba3de1cd3b
Ryan Castner <castner.rr@gmail.com>
2017-10-06 15:31:49 -0400

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='apistar_jwt',
-    version='0.1.1',
+    version='0.1.2',
     description="A JSON Web Token Component for API Star",
     long_description=readme + '\n\n' + history,
     author="Ryan Castner",
