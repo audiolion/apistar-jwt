1f5bdc83369b3a49c61b653daac38e010956b700
Ryan Castner <castner.rr@gmail.com>
2018-04-21 15:27:08 -0400

    Bump version: 0.4.1 → 0.4.2

--- apistar_jwt/__init__.py
+++ apistar_jwt/__init__.py
@@ -6,6 +6,6 @@ from apistar_jwt.decorators import anonymous_allowed, authentication_required
 
 __author__ = """Ryan Castner"""
 __email__ = 'castner.rr@gmail.com'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 __all__ = ['JWTUser', 'JWT', 'anonymous_allowed', 'authentication_required']
