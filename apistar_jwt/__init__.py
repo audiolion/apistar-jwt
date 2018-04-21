b40675baa393a157a9f46079ca305fd375598185
jgirardet <ijkl@netc.fr>
2018-04-21 19:41:00 +0200

    Update __init__.py

--- apistar_jwt/__init__.py
+++ apistar_jwt/__init__.py
@@ -1,5 +1,11 @@
 """Top-level package for apistar-jwt."""
 
+from apistar_jwt.token import JWTUser, JWT
+from apistar_jwt.decorators import anonymous_allowed, authentication_required
+
+
 __author__ = """Ryan Castner"""
 __email__ = 'castner.rr@gmail.com'
 __version__ = '0.4.1'
+
+__all__ = ['JWTUser', 'JWT', 'anonymous_allowed', 'authentication_required']
